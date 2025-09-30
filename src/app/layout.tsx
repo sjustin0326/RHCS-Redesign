d89dcb8b5787940acde717295fb3835ff10fbb47
Mabel Castillo <maby.garza@gmail.com>
2025-09-30 00:23:21 -0700

    trying tree tours collection

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,14 @@
 import type { Metadata } from 'next'
 import { Inter, Poppins, Open_Sans } from 'next/font/google'
+import localFont from 'next/font/local'
 import './globals.css'
 
-// Configure Google Fonts
+//Configure for Tekton Pro (Local FOnt used in logo)
+const tektonFont = localFont ({
+  src: './fonts/TektonPro-Bold.otf'
+})
+
+// Configure for Google Fonts
 const inter = Inter({ 
   subsets: ['latin'],
   variable: '--font-inter',
@@ -35,7 +41,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className={`${inter.variable} ${poppins.variable} ${openSans.variable}`}>
+    <html lang="en" className={`${tektonFont.className} ${inter.variable} ${poppins.variable} ${openSans.variable}`}>
       <body className="font-sans bg-background text-primary antialiased">
         {children}
       </body>
